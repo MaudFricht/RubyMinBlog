50ebb1159d885310b0373d52e6e9262f6b47d6e7
maudfricht <maud_frichement@hotmail.fr>
2018-02-16 15:10:24 +0100

    Avancée comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :posts, only: [:index, :show, :new, :create]
-  # resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :show, :create]
+  resources :comments, only: [:index, :new, :show, :create]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202153006) do
+ActiveRecord::Schema.define(version: 20180222152940) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -35,12 +35,20 @@ ActiveRecord::Schema.define(version: 20180202153006) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "username", limit: 150, null: false
-    t.string "email", null: false
-    t.string "mood", limit: 100
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "status", default: "150", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
